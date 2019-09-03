08bb5e005e23b1eb23812d0e2f5f02c8767a553d
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-09-03 18:37:14 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -5,6 +5,44 @@ const app = require('../app')
 
 const expect = chai.expect
 
+let server
+
+before(function(done) {
+  return app.up().then(_server => {
+    server = _server
+    done()
+  })
+})
+
+after(function() {
+  server.close()
+})
+
 describe('app', function() {
-  it('runs')
+  describe('up', function() {
+    it('is a function', function() {
+      expect(app.up).to.be.an.instanceof(Function)
+    })
+  })
+  describe('/user', function() {
+    describe('POST', function() {
+      it('fails with an empty request body', function(done) {
+        supertest(server).
+          post('/user').
+          expect(400, done)
+        })
+
+        it('succeeds with valid name, username, and email', function(done) {
+        supertest(server).
+          post('/user').
+          send({
+            email: 'test@email.com',
+            name: 'testName',
+            username: 'testUsername'
+          }).
+          set('content-type', 'application/json').
+          expect(200, done)
+      })
+    })
+  })
 })
--- test/models/user-test.js
+++ test/models/user-test.js
@@ -3,6 +3,7 @@ const chai = require('chai')
 const bookshelf = require('../../app/db/bookshelf')
 const User = require('../../app/models/user')
 
+const supertest = require('supertest')
 const expect = chai.expect
 
 const mockUser = {
@@ -10,3 +11,28 @@ const mockUser = {
   name: 'Name',
   username: 'username'
 }
+
+describe('User', function() {
+  let transaction;
+
+   beforeEach(done => {
+     bookshelf.transaction(t => {
+       transaction = t
+       done()
+     })
+   })
+
+   afterEach(function() {
+     return transaction.rollback()
+   })
+  it('saves a record to the database',function(){
+    return User.forge().
+   // we can use a transaction by setting
+   // a `transacting` param in the options
+   // we pass to `save()`
+   save(mockUser, { transacting: transaction }).
+   then(user => {
+     expect(user.get('id')).to.be.a('number')
+   })
+  })
+})
