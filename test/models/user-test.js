957069f655624c8d0a6d5d6397ec605e9eb2ff36
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-09-03 18:29:41 +0000

    Automatically backed up by Learn

--- test/models/user-test.js
+++ test/models/user-test.js
@@ -3,6 +3,7 @@ const chai = require('chai')
 const bookshelf = require('../../app/db/bookshelf')
 const User = require('../../app/models/user')
 
+const supertest = require('supertest')
 const expect = chai.expect
 
 const mockUser = {
